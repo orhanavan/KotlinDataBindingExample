b6d7a387e8a34576135251659f8b176f00a83820
orhan <o.avan.71@gmail.com>
2018-09-02 17:55:35 +0300

    static methods

---
+++ app/src/main/java/com/kotlin/mydatabinding/Adapter.kt
@@ -0,0 +1,19 @@
+package com.kotlin.mydatabinding
+
+import android.databinding.BindingAdapter
+import android.net.Uri
+import com.facebook.drawee.view.SimpleDraweeView
+import com.facebook.imagepipeline.request.ImageRequestBuilder
+
+class Adapter {
+    companion object {
+        @JvmStatic
+        @BindingAdapter("android:profileImage")
+        fun loadImage(view: SimpleDraweeView, url: String) {
+            val imageURI: Uri = Uri.parse(url)
+            val request = ImageRequestBuilder.newBuilderWithSource(imageURI).build()
+            view.setImageRequest(request)
+        }
+
+    }
+}
--- app/src/main/java/com/kotlin/mydatabinding/StoryItem.kt
+++ app/src/main/java/com/kotlin/mydatabinding/StoryItem.kt
@@ -2,10 +2,6 @@ package com.kotlin.mydatabinding
 
 import android.databinding.BaseObservable
 import android.databinding.Bindable
-import android.databinding.BindingAdapter
-import android.net.Uri
-import com.facebook.drawee.view.SimpleDraweeView
-import com.facebook.imagepipeline.request.ImageRequestBuilder
 
 class StoryItem(name: String?, email: String?, url: String?) : BaseObservable() {
 
@@ -27,15 +23,6 @@ class StoryItem(name: String?, email: String?, url: String?) : BaseObservable() 
 
     var profileImageUrl: String? = url
 
-    companion object {
-        @JvmStatic
-        @BindingAdapter("android:profileImage")
-        fun loadImage(view: SimpleDraweeView, url: String) {
-            val imageURI: Uri = Uri.parse(url)
-            val request = ImageRequestBuilder.newBuilderWithSource(imageURI).build()
-            view.setImageRequest(request)
-        }
 
-    }
 
 }
